70186997ad8f1ba237949184224afe2ab52be1e8
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-29 01:22:16 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
+   @@all = []
   # code goes here
+  attr_accessor:owner,:mood
+  attr_reader:name
+  def initialize(name,owner)
+      @name = name
+      @owner = owner
+      @mood = "nervous"
+      @@all << self
+      #binding.pry
+   end
+    def self.all
+       @@all
+       #binding.pry
+    end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
+require "pry"
 class Owner
+  @@all = []
+  attr_reader:species
+  attr_reader:name 
   # code goes here
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all << self
+    #binding.pry
+  end
+    def say_species
+        "I am a #{@species}."
+    end
+     def self.all
+        @@all 
+        #binding.pry
+     end
+     def self.count
+       @@all.size 
+     end
+     def self.reset_all
+       @@all.clear
+     end
+     def cats
+       
+     end
 end
